12a0fb38c3f2706c4edec61dd26fdca9f14f964f
Vladislav301 <gordeevvlad151@gmail.com>
2024-11-30 10:31:46 +0900

    Add files via upload

---
+++ 3 практическая/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class MyLibraryClass
+{
+    // Методы типа void
+    public void MethodVoid1()
+    {
+        Console.WriteLine("Метод void 1 выполнен.");
+    }
+
+    public void MethodVoid2(string message)
+    {
+        Console.WriteLine($"Метод void 2: {message}");
+    }
+
+
+    // Методы с возвращаемыми значениями
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public string GetGreeting(string name)
+    {
+        return $"Привет, {name}!";
+    }
+
+    public DateTime GetCurrentDateTime()
+    {
+        return DateTime.Now;
+    }
+}
+
+
+
